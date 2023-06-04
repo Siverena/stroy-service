6b06c50b55fb57d1e29c344dc9fd0f872e23ad9d
Siverena <vpupkin@company.com>
2023-06-04 18:47:20 +0300

    footer + map

--- src/components/ss-header/SsHeader.vue
+++ src/components/ss-header/SsHeader.vue
@@ -1,13 +1,117 @@
 <template>
   <div class="ss-header">
-    <div class="ss-header__background"></div>
-    <div class="container">
+    <div class="container ss-header__container">
       <div class="ss-header__top">
         <div class="ss-header__social">
-          <p>Подписывайся на нас <span> в соцсетях</span></p>
+          <p class="ss-header__social-sub">
+            Подписывайся на нас <br />
+            <span> в соцсетях</span>
+          </p>
+          <ssSocial />
         </div>
-        <div class="ss-header__trust"></div>
-        <div class="ss-header__call-back"></div>
+        <div class="ss-header__trust">
+          <img
+            src="../../assets/img/ss-header/shield_icon_185994.png"
+            alt=""
+            class="ss-header__trust-image"
+          />
+          <p>
+            Нам доверяют <br />
+            <span>гарантия качества</span>
+          </p>
+        </div>
+        <div class="ss-header__call-back">
+          <img
+            src="../../assets/img/ss-header/megafon.png"
+            alt=""
+            class="ss-header__call-back-image"
+          />
+          <div class="ss-header__call-back-cont">
+            <a href="tel:+77777777777" class="ss-header__call-back-tel"
+              >+7 777 777 77 77</a
+            >
+            <a href="" class="ss-header__call-back-ring">Заказать звонок</a>
+          </div>
+        </div>
+      </div>
+      <div class="ss-header__middle">
+        <div class="ss-header__logo-cont">
+          <img
+            src="../../assets/img/ss-header/logo.svg"
+            alt=""
+            class="ss-header__logo"
+          />
+          <SsBtn class="ss-header__add-task">Оставить заявку</SsBtn>
+        </div>
+        <div class="ss-header__quality">
+          <img
+            src="../../assets/img/ss-header/key.png"
+            alt=""
+            class="ss-header__quality-img"
+          />
+          <p class="ss-header__quality-text">
+            Строительсто деревянных <br />
+            домов под ключ
+          </p>
+        </div>
+        <div class="ss-header__quality">
+          <img
+            src="../../assets/img/ss-header/cup.png"
+            alt=""
+            class="ss-header__quality-img"
+          />
+          <p class="ss-header__quality-text">
+            Большой опыт <br />
+            успешной работы
+          </p>
+        </div>
+      </div>
+      <div class="ss-header__bottom">
+        <div class="ss-header__advantages">
+          <img
+            src="../../assets/img/ss-header/lightning_thunderbolt_thunder_icon_185978.png"
+            alt=""
+            class="ss-header__advantages-image"
+          />
+          <p class="ss-header__advantages-text">Собственное производство</p>
+        </div>
+        <div class="ss-header__advantages">
+          <img
+            src="../../assets/img/ss-header/pins_placeholders_location_map_markers_geolocation_address_icon_185988.png"
+            alt=""
+            class="ss-header__advantages-image"
+          />
+          <p class="ss-header__advantages-text">Работаем по всей России</p>
+        </div>
+        <div class="ss-header__advantages">
+          <img
+            src="../../assets/img/ss-header/seal_stamp_inkpad_icon_185993.png"
+            alt=""
+            class="ss-header__advantages-image"
+          />
+          <p class="ss-header__advantages-text">
+            Квалифицированные специалисты
+          </p>
+        </div>
+        <div class="ss-header__advantages">
+          <img
+            src="../../assets/img/ss-header/hand_like_thumbs_up_icon_185975.png"
+            alt=""
+            class="ss-header__advantages-image"
+          />
+          <p class="ss-header__advantages-text">
+            Гарантия <br />
+            на все!
+          </p>
+        </div>
+        <div class="ss-header__advantages">
+          <img
+            src="../../assets/img/ss-header/lock_padlock_security_icon_185979.png"
+            alt=""
+            class="ss-header__advantages-image"
+          />
+          <p class="ss-header__advantages-text">Максимальная комплектация</p>
+        </div>
       </div>
     </div>
   </div>
